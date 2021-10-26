e128eb38470a6fd3f149bd99c56db8de26846951
isadonmez <isa3can3@gmail.com>
2021-10-26 12:42:57 +0300

    18 Ekim Javascript Dersi

---
+++ src/app.js
@@ -0,0 +1,49 @@
+console.log("Merhaba Kodlama.io")
+
+let dolarBugun = 9.30 
+
+//JS type safe değildir
+
+let dolarDun = 9.20
+dolarDun="Ankara"
+
+{
+    let dolarDun=9.10
+}
+
+
+console.log(dolarDun)
+
+const euroDun =11.2
+//euroDun = 11
+console.log(euroDun)
+
+//birden fazla veriyi tek bir dizide tanımlamak için arraylarlerden yararlanılır.
+let konutKredileri = ["Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Taşıt Kredileri","Okul Bursları"]
+
+//let users = getUsersFromApi
+//Tanımlama olarak camelCasing
+//PascalCasing
+
+console.log("<ul>")
+for (let i = 0; i<konutKredileri.length; i++){
+    console.log("</li>" +konutKredileri[i]+ "</li>")
+}
+
+console.log("</ul>")
+
+
+{
+    // <ul>
+    //     <li>Konut Kredisi</li>
+    //     <li>Emlak Konut Kredisi</li>
+    //     <li>Kamu Konut Kredisi</li>
+    // </ul>
+}
+
+
+
+
+
+
+console.log(konutKredileri)
